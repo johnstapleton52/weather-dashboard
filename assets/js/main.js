4d7e89165d3a71126a4d3da65787b517a57f9c65
John Stapleton <johnstapleton52@gmail.com>
2021-09-18 20:04:26 -0700

    Refined display criteria

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,44 @@
 var cityName = document.querySelector('#inputValue');
 var search = document.querySelector('#search');
-var name = document.querySelector('#cityName');
+var namename = document.querySelector('#cityName');
 var temperature = document.querySelector('#temperature');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
-var uvIndex = document.querySelector('#uvIndex');
+
+var weatherData = document.querySelector('.weatherData');
+
 
 
 search.addEventListener('click', function() {
- // format the api url
- var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=bd16c590f9c0534397d2b37e472d5d56";
+   
+    // format the api url
+ var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&units=imperial&appid=bd16c590f9c0534397d2b37e472d5d56";
 
 
  // make request to URL
  fetch(apiUrl)
+   
     .then(response => response.json())
+    // .then(data => console.log(data))
     .then(data => {
+        // Parse out and name data for each desired feature
         var nameValue = data['name'];
-        var tempValue = data['main']['temp'];
-        var windSpeed = data['wind']['speed'];
-
-        cityName.innerHTML = nameValue;
+        var tempVal = data['main']['temp'] +"°F";
+        var windSpeed = data['wind']['speed'] +"mph";
+        var humidityVal = data['main']['humidity'] +"%";
         
+   
+        // display data in respective divs
+        namename.innerHTML += nameValue;
+        temperature.innerHTML += tempVal;
+        wind.innerHTML += windSpeed;
+        humidity.innerHTML += humidityVal;
     })
-    .then(data => console.log(data))
+    
+    
 
 // alert user if the city is not found
     .catch(err => alert("Could not identify city name"))
 });
    
 
-
-
-// "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=bd16c590f9c0534397d2b37e472d5d56"
