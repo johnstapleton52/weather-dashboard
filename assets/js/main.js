3b5bc1e976ce46fe62ff88243c968c1a8aff495a
John Stapleton <johnstapleton52@gmail.com>
2021-09-18 18:45:42 -0700

    Completed basic api call functioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
-var getLatLong = function () {
-    // Enter city name to return latitude and longitude
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=$bd16c590f9c0534397d2b37e472d5d56");
+var cityName = document.querySelector('#inputValue');
+var search = document.querySelector('#search');
+var name = document.querySelector('#cityName');
+var temperature = document.querySelector('#temperature');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
+var uvIndex = document.querySelector('#uvIndex');
 
-    // Enter latitude and longitude to return weather data
-};
 
-getLatLong();
+search.addEventListener('click', function() {
+ // format the api url
+ var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=bd16c590f9c0534397d2b37e472d5d56";
+
+
+ // make request to URL
+ fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var windSpeed = data['wind']['speed'];
+
+        cityName.innerHTML = nameValue;
+        
+    })
+    .then(data => console.log(data))
+
+// alert user if the city is not found
+    .catch(err => alert("Could not identify city name"))
+});
+   
+
 
 
 // "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=bd16c590f9c0534397d2b37e472d5d56"
